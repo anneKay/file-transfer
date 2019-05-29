9130651d2d0c2c5a449134ce13caea943432c2f0
Kalu Nwanna <nwannauk@gmail.com>
2019-05-29 21:39:15 +0100

    - initial commit - build ui pages - integrate devise for authentication

---
+++ db/migrate/20190529120056_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :name
+      t.integer :user_id
+
+      t.timestamps
+    end
+    add_foreign_key :posts, :users
+  end
+end
